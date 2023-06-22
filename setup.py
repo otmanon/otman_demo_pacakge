021c7a057f00c4fc336a7799925acfa68032e12e
otmanon <otman.benchekroun@mail.utoronto.ca>
2023-06-22 02:54:57 -0400

    otman demo package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 import re
 
 # Extract the version from the init file.
-VERSIONFILE="relevantpackage/__init__.py"
+VERSIONFILE="demo_otman_package/__init__.py"
 getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
 if getversion:
     new_version = getversion.group(1)
@@ -16,15 +16,15 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      install_requires=['matplotlib','numpy','pandas'],        # Dependencies
      python_requires='>=3',                                   # Minimum Python version
-     name='relevantpackage',                                  # Package name
+     name='demo_otman_package',                                  # Package name
      version=new_version,                                     # Version
      author="Otman Benchekroun",                                     # Author name
      author_email="otman.benchekroun@mail.utoronto.ca",                           # Author mail
-     description="Python package for my relevantpackage.",    # Short package description
+     description="Python package for my demo_otman_package.",    # Short package description
      long_description=long_description,                       # Long package description
      long_description_content_type="text/markdown",
      url="https://github.com/otmanon/otman_demo_package.gite",       # Url to your Git Repo
-     download_url = 'https://github.com/erdogant/relevantpackage/archive/'+new_version+'.tar.gz',
+     download_url = 'https://github.com/otmanon/demo_otman_package/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(),                     # Searches throughout all dirs for files to include
      include_package_data=True,                               # Must be true to include files depicted in MANIFEST.in
      license_files=["LICENSE"],                               # License file
